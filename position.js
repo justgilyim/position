3684711b53de2ea83d7b9c8ba1b45531821b01e4
Gilbert <gyim@hawaii.edu>
2015-10-05 18:02:28 -1000

    Added jasmine test

--- position.js
+++ position.js
@@ -1,7 +1,15 @@
 /**
  * Created by Gilbert on 9/22/2015.
  */
-var test = [1,3,5,6];
+
+/* exported position */
+
+/**
+ * Finds the position in the array of numss where target should be inserted.
+ * @param nums The array of numbers
+ * @param target The number to be inserted
+ * @returns {number} The position where it should be inserted
+ */
 function position(nums, target){
     var pointer = 0;
     while(nums[pointer] <= target && pointer < nums.length) {
@@ -12,8 +20,8 @@ function position(nums, target){
     }
     return pointer;
 }
+
 console.log(position(test, 5));
 console.log(position(test, 2));
 console.log(position(test, 7));
 console.log(position(test, 0));
-/** time: 8:39 **/
